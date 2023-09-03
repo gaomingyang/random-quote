ab602fec46c63f8dd1c8ba0553cadb17e99cd3b5
Mingyang Gao <gaomingyangcn@gmail.com>
2023-09-03 10:55:40 -0600

    component and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+// import './App.css';
+import './App.scss';
 
 function App() {
+  const [text,setText] = useState([]);
+  //更新内容
+  function clickNewQuote(event) {
+    console.log("click new quote")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <i className="fa-solid fa-quote-left"></i>
+        <span id="text">
+          Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.
+        </span>
+        <i className="fa-solid fa-quote-right"></i>
+
+        <div id="author"> - Robert Frost</div>
+
+        <button id="new-quote" onClick={clickNewQuote}>New Quote</button>
+        <div id="share-bar">
+          <a id="tweet-quote" href='https://twitter.com/intent/tweet' target="_blank">
+            <i className="fa-brands fa-square-x-twitter"></i>
+          </a>
+        </div>
+        
+      </div>
     </div>
   );
 }
